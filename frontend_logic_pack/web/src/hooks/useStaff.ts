16b043776bd3dba178a1202b9ad7923033b78919
gth27 <gthanh919@gmail.com>
2025-12-22 00:41:46 +0700

    Update useStaff.ts

--- frontend_logic_pack/web/src/hooks/useStaff.ts
+++ frontend_logic_pack/web/src/hooks/useStaff.ts
@@ -5,7 +5,7 @@ import { CONFIG } from '../config';
 export function useStaff() {
   const { mutate: signAndExecute } = useSignAndExecuteTransactionBlock();
 
-  // boothCapId: Must be provided by the staff user (hardcoded for now is fine for hackathon)
+  // boothCapId: Must be provided by the staff user
   const stampTicket = (boothCapId: string, ticketId: string) => {
     const tx = new TransactionBlock();
     const CLOCK = '0x6';
